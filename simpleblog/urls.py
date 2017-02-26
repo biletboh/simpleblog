0fc86cbc075b9adf4178077c1b86c0bf0f0d914a
bb <biletskyboh@gmail.com>
2017-02-26 09:04:36 +0200

    basic project setup + email confirmation

---
+++ simpleblog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    # basic urls
+    url(r'^', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    
+    # email verification urls
+    url(r'^accounts/', include('allauth.urls')),
+]
