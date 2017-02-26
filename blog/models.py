1a21646e8636a9809fe34ae5e18366b27f0b4713
bb <biletskyboh@gmail.com>
2017-02-26 10:25:41 +0200

    fixed setup and messages of email verification

--- blog/models.py
+++ blog/models.py
@@ -13,21 +13,21 @@ from easy_thumbnails.fields import ThumbnailerImageField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-#class UserProfile(models.Model):
-#    user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, primary_key=True, on_delete=models.CASCADE, related_name='user_profile')
-#    avatar = ThumbnailerImageField(upload_to='profile_images', blank=True)
-#    birthday = models.DateField(null=True, blank=True)
-#    country = models.CharField(max_length = 128, blank=True)
-#    city = models.CharField(max_length = 128, blank=True)
-#
-#    def __unicode__(self):
-#        return self.user.username
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, primary_key=True, on_delete=models.CASCADE, related_name='user_profile')
+    avatar = ThumbnailerImageField(upload_to='profile_images', blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length = 128, blank=True)
+    city = models.CharField(max_length = 128, blank=True)
 
-#@receiver(post_save, sender=User)
-#def create_user_profile(sender, instance, created, **kwargs):
-#    if created:
-#        UserProfile.objects.create(user=instance)
-#
-#@receiver(post_save, sender=User)
-#def save_user_profile(sender, instance, **kwargs):
-#    instance.user_profile.save()
+    def __unicode__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.user_profile.save()
